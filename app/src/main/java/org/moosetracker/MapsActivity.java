aedd3dae2d5a0f3cb8eaac882946a6beb4ba2188
Sam <srgrosenick@gmail.com>
2018-04-21 16:53:48 -0800

    Updated activity to include button that opens CreateSighting

--- app/src/main/java/org/moosetracker/MapsActivity.java
+++ app/src/main/java/org/moosetracker/MapsActivity.java
@@ -1,19 +1,33 @@
 package org.moosetracker;
 
-import android.app.ActionBar;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MapsActivity extends AppCompatActivity {
 
-    DatabaseHelper myDb;
+    //DatabaseHelper myDb;
+    private Button mCreateSighting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // This creates our database
-        myDb = new DatabaseHelper(this);
+        //myDb = new DatabaseHelper(this);
+
+        mCreateSighting = (Button) findViewById(R.id.create_sighting_button);
+        mCreateSighting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MapsActivity.this, CreateSighting.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
 }
