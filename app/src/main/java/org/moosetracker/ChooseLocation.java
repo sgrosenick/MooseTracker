85d2cf927aaa7d3f2fdf60463d64acffc70a5899
Sam <srgrosenick@gmail.com>
2018-04-22 11:40:05 -0800

    Added functionality to create marker

--- app/src/main/java/org/moosetracker/ChooseLocation.java
+++ app/src/main/java/org/moosetracker/ChooseLocation.java
@@ -1,18 +1,26 @@
 package org.moosetracker;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 
 public class ChooseLocation extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +31,40 @@ public class ChooseLocation extends FragmentActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("This Marker is in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Get permission to access user's location
+        if (ActivityCompat.checkSelfPermission(ChooseLocation.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ChooseLocation.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            if (!mMap.isMyLocationEnabled()){
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+
+        mMap.setOnMapClickListener(new OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                MarkerOptions options = new MarkerOptions()
+                        .position(latLng)
+                        .title("Moose")
+                        .snippet("There is a moose here");
+                marker = mMap.addMarker(options);
+            }
+        });
+
+        // Add a marker in Anchorage and move the camera
+        LatLng anchorage = new LatLng(61.2181, -149.9003);
+        mMap.addMarker(new MarkerOptions().position(anchorage).title("Anchorage!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(anchorage, 7));
     }
+
 }
