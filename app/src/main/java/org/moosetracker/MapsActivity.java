248879a09925e6be889fdfd4b87434e8b603e8ae
Sam <srgrosenick@gmail.com>
2018-04-22 11:43:40 -0800

    Updated for map testing

--- app/src/main/java/org/moosetracker/MapsActivity.java
+++ app/src/main/java/org/moosetracker/MapsActivity.java
@@ -1,8 +1,11 @@
 package org.moosetracker;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +31,5 @@ public class MapsActivity extends AppCompatActivity {
             }
         });
     }
-
 
 }
