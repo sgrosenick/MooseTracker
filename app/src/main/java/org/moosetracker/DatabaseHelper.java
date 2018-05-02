b8e03d80e2a92de4766092dffaa68190f4900665
Sam <srgrosenick@gmail.com>
2018-05-01 18:35:59 -0800

    Updated to include create database method

--- app/src/main/java/org/moosetracker/DatabaseHelper.java
+++ app/src/main/java/org/moosetracker/DatabaseHelper.java
@@ -19,6 +19,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
